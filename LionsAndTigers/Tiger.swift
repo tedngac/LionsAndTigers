bcb038e01cfe8f45e1bf64b43a0509f15540196e
Ted Ngac <duy.a.ngac@icloud.com>
2015-10-22 21:03:33 +0800

    first commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,9 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Ngac Duy Anh on 10/22/15.
+//  Copyright © 2015 Ngac Duy Anh. All rights reserved.
+//
+
+import Foundation
---
+++ LionsAndTigers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LionsAndTigers
+//
+//  Created by Ngac Duy Anh on 10/22/15.
+//  Copyright © 2015 Ngac Duy Anh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
